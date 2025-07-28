1c12780b6e30c575ea0ec8deca9a06c4273debe7
Meet6174 <jairamdesai18184@gmail.com>
2025-07-28 21:06:56 +0530

    Add files via upload

---
+++ list.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main()
+{
+  list<int> l;
+  l.push_back(3);
+  l.push_back(4);
+  l.push_front(2);
+  l.push_front(1);
+for (int x : l)
+{
+    cout<<x<<" ";
+}
+    return 0;
+}
