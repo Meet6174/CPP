1269679adc69a4483087fe0cc66da1bc60d3e745
Meet6174 <jairamdesai18184@gmail.com>
2025-08-04 01:01:55 +0530

    Add files via upload

---
+++ deque.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<deque>//dynemic arrays,accessed randomly eliment
+using namespace std;
+int main()
+{
+  deque<int> d;
+  d.push_back(3);
+  d.push_back(4);
+  d.push_front(2);
+  d.push_front(1);
+for (int x : d)
+{
+    cout<<x<<" ";
+}
+    return 0;
+}
